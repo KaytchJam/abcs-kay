02bc2dd606fa9e4e95a25722c0ba369bb43006fe
Jean-Claude Bissou <jeanclaudebissou@gmail.com>
2024-08-17 15:41:37 -0500

    home and sponsor page down, needs images

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import NavBar from "./components/NavBar";
+import AboutPage from './components/AboutPage';
+import SponsorsPage from './components/SponsorsPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/home"/>} />
+        <Route path="/home" element={<HomePage/>} />
+        <Route path="/about" element={<AboutPage/>} />
+        <Route path="/sponsors" element={<SponsorsPage/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import logo from '../assets/abcs.jpeg'; // Make sure to import your logo
+
+function NavBar() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" fixed="top" className="py-3">
+      <Container>
+        <Navbar.Brand href="/home">
+          <img
+            alt="ABCS Logo"
+            src={logo}
+            width="60"
+            height="30"
+            className="d-inline-block align-top"
+          />
+          {' Texas ABCS'}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/gallery">Gallery</Nav.Link>
+            <Nav.Link href="/calendar">Calendar</Nav.Link>
+            <Nav.Link href="/sponsors">Sponsors</Nav.Link>
+            <Nav.Link href="/members">Members</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
