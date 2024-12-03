dc7232c72d755f616c7fb52d833e42dd1529abda
Jean-Claude Bissou <jeanclaudebissou@gmail.com>
2024-12-03 13:02:51 -0600

    calendar page and title//logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import HomePage from "./components/HomePage";
 import NavBar from "./components/NavBar";
 import AboutPage from './components/AboutPage';
 import SponsorsPage from './components/SponsorsPage';
+import CalendarPage from './components/CalendarPage';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Navigate replace to="/home"/>} />
         <Route path="/home" element={<HomePage/>} />
         <Route path="/about" element={<AboutPage/>} />
+        <Route path="/calendar" element={<CalendarPage/>} />
         <Route path="/sponsors" element={<SponsorsPage/>} />
       </Routes>
     </BrowserRouter>
