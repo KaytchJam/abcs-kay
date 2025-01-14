e83f9a9d68960fb988947987ae9cb4e54effa404
Jean-Claude Bissou <jeanclaudebissou@gmail.com>
2025-01-14 14:33:22 -0600

    api images test

--- backend/api.py
+++ backend/api.py
@@ -1,11 +1,14 @@
 from flask import Flask
 import requests
 from flask_cors import CORS
-import ssl
+import boto3
+
+officers = ['boueny', 'clint', 'essie', 'gary', 'isaac', 'jean-claude', 'jerry', 'kenna', 'kevin', 'mick', 'robert']
 
 app = Flask(__name__)
 CORS(app)
 ec2_api = 'http://18.116.76.42:3000/groups/0/'
+s3_client = boto3.client('s3')
 
 @app.route("/")
 def hello_world():
@@ -37,6 +40,26 @@ def members():
             "members": list_members}
   else:
     return {"error": "Failed to fetch members"}
+  
+
+def generate_presigned_url(bucket_name, object_key, expiration_seconds=3600):
+
+    """ Generates a pre-signed URL for an S3 object """
+
+    return s3_client.generate_presigned_url(
+        'get_object',
+        Params={'Bucket': bucket_name, 'Key': object_key},
+        ExpiresIn=expiration_seconds
+    )
+  
+@app.route("/images")
+def images():
+  response = {}
+
+  for name in officers:
+    response[name] = generate_presigned_url('abcs-bucket', 'officer-pictures/' + name + '.jpg')
+
+  return response
   
 if __name__ == "__main__":
     app.run(debug=True, port=9000)
